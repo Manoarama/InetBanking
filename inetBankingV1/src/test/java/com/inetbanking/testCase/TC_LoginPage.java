e07a07a74d9feb1c97de20d2cee89b6f10d05782
WRAGG5 <wraggstest123@gmail.com>
2023-09-19 17:34:05 +0530

    first Commit

---
+++ inetBankingV1/src/test/java/com/inetbanking/testCase/TC_LoginPage.java
@@ -0,0 +1,11 @@
+package com.inetbanking.testCase;
+
+import org.testng.annotations.Test;
+
+public class TC_LoginPage extends BaseClass{
+	
+	@Test
+	public void LoginTest() {
+	driver.get(baseURL);	
+	}
+}
